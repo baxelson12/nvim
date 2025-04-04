3af17b326a3ce7a2d0b6928282ceb48931f307d1
baxelson12 <bradaxelson@gmail.com>
2025-04-04 11:12:22 -0400

    fix: Enter breaks commands

--- lua/after/plugins/cmp.lua
+++ lua/after/plugins/cmp.lua
@@ -52,17 +52,18 @@ cmp.setup({
       end
     end, { "i", "s" }),
 
-    ["<CR>"] = cmp.mapping({
-      i = function(fallback)
-        if cmp.visible() and cmp.get_active_entry() then
-          cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-        else
-          fallback()
-        end
-      end,
-      s = cmp.mapping.confirm({ select = true }),
-      c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-    }),
+    -- Not working the way i'd like
+    -- ["<CR>"] = cmp.mapping({
+    --   i = function(fallback)
+    --     if cmp.visible() and cmp.get_active_entry() then
+    --       cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+    --     else
+    --       fallback()
+    --     end
+    --   end,
+    --   s = cmp.mapping.confirm({ select = true }),
+    --   c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+    -- }),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
