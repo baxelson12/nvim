4705882f6deda21de37441fc3bc1cc31cf2cd03e
baxelson12 <bradaxelson@gmail.com>
2025-06-23 20:16:45 -0400

    feat: Autotag, better indenting, cursorword, trouble

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,33 +1,8 @@
-return {
-  "lukas-reineke/indent-blankline.nvim",
-  main = "ibl",
-  opts = {},
+return
+{
+  'echasnovski/mini.indentscope',
+  version = '*',
   config = function()
-    -- Brighter colors
-    -- local hooks = require("ibl.hooks")
-    -- local highlight = {
-    --   "RainbowRed",
-    --   "RainbowYellow",
-    --   "RainbowBlue",
-    --   "RainbowOrange",
-    --   "RainbowGreen",
-    --   "RainbowViolet",
-    --   "RainbowCyan",
-    -- }
-    -- hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-    --   -- These colors are from the Dracula palette, selected to fit a "soft" theme.
-    --   vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#FF5555" })    -- Red
-    --   vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#F1FA8C" }) -- Yellow
-    --   vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#BD93F9" })   -- Purple
-    --   vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#FFB86C" }) -- Orange
-    --   vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#50FA7B" })  -- Green
-    --   vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#FF79C6" }) -- Pink
-    --   vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#8BE9FD" })   -- Cyan
-    -- end)
-    --
-    -- require("ibl").setup({
-    --   indent = { highlight = highlight }
-    -- })
-    require("ibl").setup({})
+    require("mini.indentscope").setup()
   end
 }
