b014e3c7c50ea663d89124a4d2e8b0b2a0b23c60
ttl <bradaxelson@gmail.com>
2026-01-11 04:45:36 +0000

    feat: New colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,16 @@
 return {
-  {
-    "Mofiqul/dracula.nvim",
-    config = function()
-      vim.cmd [[colorscheme dracula-soft]]
-    end,
+  -- {
+  --   "Mofiqul/dracula.nvim",
+  --   config = function()
+  --     vim.cmd [[colorscheme dracula-soft]]
+  --   end,
+  -- },
+  { 
+    "catppuccin/nvim", 
+    name = "catppuccin", 
+    priority = 1000, 
+    config = function() 
+      vim.cmd [[colorscheme catppuccin-frappe]]
+    end
   },
 }
